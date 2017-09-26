c07b26b07d1b95348599d510ebf0aa73b760937b
liubintao <240225241@qq.com>
2017-09-26 21:56:10 +0800

    test

--- java-basis/src/main/java/com.robust/base/StringTest.java
+++ java-basis/src/main/java/com.robust/base/StringTest.java
@@ -12,5 +12,11 @@ public class StringTest {
 
         System.out.println(a.getBytes().length);
         System.out.println("刘".getBytes().length);
+
+        System.out.println(String.format("%02d", 1));
+
+        System.out.println(String.format("%020d", 1));
+
+        System.out.println("20170922".compareTo("20170923"));
     }
 }
